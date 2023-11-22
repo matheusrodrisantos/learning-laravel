30ccdb132368c9a16c18910df485ca9427c24f8b
Matheus Rodrigues dos Santos <matheus.rodrisantos@outlook.com>
2023-11-22 15:09:25 -0300

    add icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,12 +16,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" >
+    <link rel="apple-touch-icon" href="{{ asset('icons/apple-touch-icon.png')}}" sizes="180x180">
 
-    
+    <link rel="icon" href="{{ favicon-32x32.png" sizes="32x32" type="image/png">
 
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+
 <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
 <link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json">
 <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
